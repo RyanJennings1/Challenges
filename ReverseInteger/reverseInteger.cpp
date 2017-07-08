211bf94bc76c28aacc3650a6825d77e8b7f4b5a4
Ryan Jennings <ryanjennings1010@gmail.com>
2017-07-09 00:34:34 +0100

    Update .gitignore and add ReverseInteger solution in cpp

---
+++ ReverseInteger/reverseInteger.cpp
@@ -0,0 +1,53 @@
+/*
+ * Reverse digits of an integer.
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321
+ *
+ * Note:
+ * The input is assumed to be a 32-bit signed integer.
+ * Your function should return 0 when the reversed
+ * integer overflows.
+ *
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    int reverse(int x) {
+      int returnInt = 0;
+      int y = x;
+      int i;
+
+      int intLength = log10(abs(x)) + 1;
+      int individualNumbers[intLength];
+      int exponent = intLength - 1;
+
+      for(i = 0; i < intLength; i++) {
+        individualNumbers[i] = y%(10);
+        returnInt += (individualNumbers[i]*(pow(10, exponent)));
+        y = y/10;
+        exponent--;
+      }
+
+      return returnInt;
+    }
+
+};
+
+int main(int argc, char* argv[]) {
+  Solution sol1;
+
+  int solutionInt = sol1.reverse(atoi(argv[1]));
+
+  cout << "Solution: " << solutionInt << endl;
+
+  return 0;
+}
+
+
